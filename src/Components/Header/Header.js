c1cdf08307ebe4a9608f4af802cbc33bc7d2ec33
dariuszb <dariusz.berer@gmail.com>
2021-01-03 10:54:44 +0100

    banner anim

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Logo from "../../Assets/logo_front.png";
 import Menu from "./HeaderComponents/Menu";
 import "./HeaderStyle.css";
+import { useQuery } from "react-apollo";
+import gql from "graphql-tag";
+const EXCHANGE_RATES = gql`
+  query MyQuery {
+    menus {
+      edges {
+        node {
+          menuItems {
+            edges {
+              node {
+                label
+                path
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 const Header = () => {
+  const { error, data } = useQuery(EXCHANGE_RATES);
+  if (error) return <p>Error :(</p>;
   return (
     <section className="header">
-      <div className="header__content">
-        <a href="/">
-          <img className="header__logo" src={Logo} alt="ym" />
-        </a>
-        <Menu />
-      </div>
+      {data ? (
+        <div className="header__content">
+          <a href="/">
+            <img className="header__logo" src={Logo} alt="ym" />
+          </a>
+          <Menu menuItems={data} />
+        </div>
+      ) : null}
     </section>
   );
 };
--- src/Components/Header/HeaderComponents/Menu.js
+++ src/Components/Header/HeaderComponents/Menu.js
@@ -1,32 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { useQuery } from "react-apollo";
-import gql from "graphql-tag";
-const EXCHANGE_RATES = gql`
-  query MyQuery {
-    menus {
-      edges {
-        node {
-          menuItems {
-            edges {
-              node {
-                label
-                path
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
-const Menu = () => {
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
-  if (error) return <p>Error :(</p>;
+import React from "react";
 
+const Menu = ({ menuItems }) => {
   return (
     <ul className="menu">
-      {data
-        ? data.menus.edges[0].node.menuItems.edges.map(function (item, i) {
+      {menuItems
+        ? menuItems.menus.edges[0].node.menuItems.edges.map(function (item, i) {
             return (
               <li key={i} className="menu__element">
                 <a className="menu__element__link" href={item.node.path}>
